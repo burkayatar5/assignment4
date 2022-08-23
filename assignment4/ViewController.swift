1c2aa877d4f96725da10a42701925081fdb996b1
Burkay <burkayatar5@gmail.com>
2022-08-23 14:11:37 +0300

    Initial Commit

---
+++ assignment4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment4
+//
+//  Created by Burkay Atar on 23.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
